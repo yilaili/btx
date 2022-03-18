2887c6adb991c1fc4090372c65bcb8e66bd5ad4e
fredericpoitevin <frederic.poitevin@gmail.com>
2022-03-17 19:15:22 -0700

    Added first draft of elog submission script.

---
+++ scripts/elog_submit.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Adapted from Silke's script.
+# TODO: ask for permission to use.
+
+usage()
+{
+cat << EOF
+$(basename "$0"):
+  Script to launch python scripts from the eLog
+
+  OPTIONS:
+    -h|--help
+      Definition of options
+    -c|--config
+      Input config file
+    -q|--queue
+      Queue to use on SLURM
+EOF
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+  case $key in
+    -h|--help)
+      usage
+      exit
+      ;;
+    -q|--queue)
+      QUEUE="$2"
+      shift
+      shift
+      ;;
+    -c|--config)
+      CONFIGFILE="$2"
+      shift
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+    esac
+done
+set -- "${POSITIONAL[@]}"
+
+QUEUE=${QUEUE:='psanaq'}
+CORES=32
+
+#Submit to SLURM
+sbatch << EOF
+#!/bin/bash
+
+#SBATCH -p ${QUEUE}
+#SBATCH -t 10:00:00
+#SBATCH --exclusive
+#SBATCH --job-name "task"
+#SBATCH --ntasks=$CORES
+
+source /reg/g/psdm/etc/psconda.sh -py3
+
+echo "$MAIN_PY $CONFIGFILE"
+$MAIN_PY $CONFIGFILE
+EOF
+
+echo "Job sent to queue"
