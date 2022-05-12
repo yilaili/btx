9a008a13dfbf5d8337ca5d33bf92f982fe895c3e
Ariana Peck <apeck@psanagpu112.pcdsn>
2022-05-12 16:26:23 -0700

    adding reporting functionality to Indexer class

--- btx/processing/indexer.py
+++ btx/processing/indexer.py
@@ -1,10 +1,15 @@
 import numpy as np
 import argparse
 import os
+import subprocess
+import requests
 
 class Indexer:
     
-    """ Index cxi files using indexamajig: https://www.desy.de/~twhite/crystfel/manual-indexamajig.html """
+    """ 
+    Wrapper for writing executable to index cxi files using CrystFEL's indexamajig 
+    and reporting those results to a summary file and the elog.
+    """
 
     def __init__(self, exp, run, det_type, tag, taskdir, geom, cell=None, int_rad='4,5,6', methods='mosflm',
                  tolerance='5,5,5,1.5', no_revalidate=True, multi=True, profile=True):
@@ -57,6 +62,8 @@ class Indexer:
             self.tmp_exe = os.environ['TMP_EXE']
         else:
             self.tmp_exe = os.path.join(taskdir ,f'r{self.run:04}/index_r{self.run:04}.sh')
+        self.peakfinding_summary = os.path.join(taskdir ,f'r{self.run:04}/peakfinding.summary')
+        self.indexing_summary = os.path.join(taskdir ,f'r{self.run:04}/indexing.summary')
         
     def write_exe(self):
         """
@@ -73,6 +80,43 @@ class Indexer:
                 f.write("#!/bin/bash\n")
                 f.write(f"{command}\n")
             print(f"Indexing executable written to {self.tmp_exe}")
+            
+    def report(self, update_url=None):
+        """
+        Write results to a .summary file and optionally post to the elog.
+        
+        Parameters
+        ----------
+        update_url : str
+            elog URL for posting progress update
+        """
+        if self.rank == 0:
+            # retrieve number of indexed patterns
+            command = ["grep", "Cell parameters", f"{self.stream}"]
+            output,error  = subprocess.Popen(
+                                command, universal_newlines=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            n_indexed = len(output.split('\n')[:-1])
+            
+            # retrieve number of total patterns
+            command = ["grep", "Number of hits found", f"{self.peakfinding_summary}"]
+            output,error  = subprocess.Popen(
+                                command, universal_newlines=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            n_total = int(output.split(":")[1].split("\n")[0])
+            
+            # write summary file
+            with open(self.indexing_summary, 'w') as f:
+                f.write(f"Number of indexed events: {n_indexed}\n")
+                f.write(f"Fractional indexing rate rate: {(n_indexed/n_total):.2f}\n")
+
+            # post to elog
+            if update_url is not None:
+                try:
+                    requests.post(update_url, json=[{ "key": "Number of indexed events", "value": f"{n_indexed}"},
+                                                    { "key": "Fractional indexing rate", "value": f"{(n_indexed/n_total):.2f}"}, ])
+                except:
+                    print("Could not communicate with the elog update url")
 
 def parse_input():
     """
@@ -82,8 +126,11 @@ def parse_input():
     parser.add_argument('-e', '--exp', help='Experiment name', required=True, type=str)
     parser.add_argument('-r', '--run', help='Run number', required=True, type=int)
     parser.add_argument('-d', '--det_type', help='Detector name, e.g epix10k2M or jungfrau4M', required=True, type=str)
+    parser.add_argument('--tag', help='Suffix extension for stream file', required=True, type=str)
     parser.add_argument('--taskdir', help='Base directory for indexing results', required=True, type=str)
-    parser.add_argument('--geom', help='CrystFEL-style geom file', required=True, type=str)
+    parser.add_argument('--report', help='Report indexing results to summary file and elog', action='store_true')
+    parser.add_argument('--update_url', help='URL for communicating with elog', required=False, type=str)
+    parser.add_argument('--geom', help='CrystFEL-style geom file, required if not reporting', required=False, type=str)
     parser.add_argument('--cell', help='File containing unit cell information (.pdb or .cell)', required=False, type=str)
     parser.add_argument('--int_rad', help='Integration radii for peak, buffer and background regions', required=False, type=str, default='4,5,6')
     parser.add_argument('--methods', help='Indexing methods', required=False, type=str, default='xgandalf,mosflm,xds')
@@ -97,7 +144,10 @@ def parse_input():
 if __name__ == '__main__':
     
     params = parse_input()
-    indexer_obj = Indexer(exp=params.exp, run=params.run, det_type=params.det_type, taskdir=params.taskdir, geom=params.geom, 
+    indexer_obj = Indexer(exp=params.exp, run=params.run, det_type=params.det_type, tag=params.tag, taskdir=params.taskdir, geom=params.geom, 
                           cell=params.cell, int_rad=params.int_rad, methods=params.methods, tolerance=params.tolerance, 
                           no_revalidate=params.no_revalidate, multi=params.multi, profile=params.profile)
-    indexer_obj.write_exe()
+    if not params.report:
+        indexer_obj.write_exe()
+    else:
+        indexer_obj.report(params.update_url)
