6ae18c68f2868f819229b5de8aadbef18a53f4c2
fredericpoitevin <frederic.poitevin@gmail.com>
2022-05-11 19:02:27 -0700

    DAG clean up. Created optimize_geometry DAG.

---
+++ btx/interfaces/html_interface.py
@@ -0,0 +1,3 @@
+
+
+class HTMLInterface:
--- dags/optimize_geometry.py
+++ dags/optimize_geometry.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from plugins.jid import JIDSlurmOperator
 
 # DAG SETUP
-description='BTX detector distance estimation DAG'
+description='BTX optimize geometry DAG'
 dag_name = os.path.splitext(os.path.basename(__file__))[0]
 
 dag = DAG(
@@ -16,11 +16,8 @@ dag = DAG(
 
 
 # Tasks SETUP
-task_id='run_analysis'
-run_analysis = JIDSlurmOperator( task_id=task_id, dag=dag)
-
-task_id='opt_distance'
-opt_distance = JIDSlurmOperator( task_id=task_id, dag=dag)
+task_id='opt_geom'
+opt_geom = JIDSlurmOperator( task_id=task_id, dag=dag, run_at='SRCF_FFB')
 
 # Draw the DAG
-run_analysis >> opt_distance
+opt_geom
