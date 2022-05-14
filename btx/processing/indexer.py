ccf45339cafbedb52106eb6002b091fe7949fe1b
fredericpoitevin <frederic.poitevin@gmail.com>
2022-05-13 17:48:46 -0700

    `tag_cxi` was not provided to report in `Indexer`.

--- btx/processing/indexer.py
+++ btx/processing/indexer.py
@@ -117,6 +117,7 @@ class Indexer:
             output,error  = subprocess.Popen(
                                 command, universal_newlines=True,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            print(output)
             n_total = int(output.split(":")[1].split("\n")[0])
             
             # write summary file
