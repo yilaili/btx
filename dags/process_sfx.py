079458132f65da336c3e648e2c301615afb05af1
fredericpoitevin <frederic.poitevin@gmail.com>
2022-07-28 16:04:00 -0700

    Created process_sfx DAG

---
+++ dags/process_sfx.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import os
+from airflow import DAG
+from plugins.jid import JIDSlurmOperator
+
+# DAG SETUP
+description='BTX process SFX DAG'
+dag_name = os.path.splitext(os.path.basename(__file__))[0]
+
+dag = DAG(
+    dag_name,
+    start_date=datetime( 2022,4,1 ),
+    schedule_interval=None,
+    description=description,
+  )
+
+
+# Tasks SETUP
+task_id='run_analysis'
+run_analysis = JIDSlurmOperator( task_id=task_id, dag=dag, run_at='SRCF_FFB')
+
+task_id='find_peaks'
+find_peaks = JIDSlurmOperator( task_id=task_id, dag=dag, run_at='SRCF_FFB')
+
+task_id='index'
+index = JIDSlurmOperator( task_id=task_id, dag=dag, run_at='SRCF_FFB')
+
+task_id='stream_analysis'
+stream_analysis = JIDSlurmOperator( task_id=task_id, dag=dag, run_at='SRCF_FFB')
+
+# Draw the DAG
+run_analysis
+find_peaks >> index >> stream_analysis
